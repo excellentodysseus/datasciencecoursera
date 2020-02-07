42fbef59dc6e4472c4160220f4291cd4e752867e
excellentodysseus <52474309+excellentodysseus@users.noreply.github.com>
2020-02-07 10:53:56 -0800

    Add files via upload

---
+++ project2.R
@@ -0,0 +1,33 @@
+
+
+makeCacheMatrix <- function(x = matrix()){
+  inverse <- NULL
+  set <-  function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(the_inverse) inverse <<- the_inverse
+  getInverse <- function() inverse
+  list(set = set, get = get, 
+        setInverse = setInverse,
+        getInverse = getInverse)
+}
+
+cacheSolve <- function(x,...){
+  
+  inverse <- x$getInverse()
+  if (!is.null(inverse)){
+    
+    message("geting cached data")
+    return(inverse)
+  
+    }
+  data <- x$get()
+  inverse <- solve(data,...)
+  x$setInverse(inverse)
+  
+  return(inverse)
+}
+
+
